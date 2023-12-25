f3de49351de360b5ce9b7c22766f0892c337c17e
Catherine Xie <catherinexie2014@gmail.com>
2023-12-24 21:31:13 -0500

    Create GCDofStrings.py

---
+++ Easy/GCDofStrings.py
@@ -0,0 +1,42 @@
+# For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+# Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+# Example 1:
+
+# Input: str1 = "ABCABC", str2 = "ABC"
+# Output: "ABC"
+# Example 2:
+
+# Input: str1 = "ABABAB", str2 = "ABAB"
+# Output: "AB"
+# Example 3:
+
+# Input: str1 = "LEET", str2 = "CODE"
+# Output: ""
+ 
+
+# Constraints:
+
+# 1 <= str1.length, str2.length <= 1000
+# str1 and str2 consist of English uppercase letters.
+
+import fractions 
+# python3 uses math library
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 == str2:
+            return str1
+        if str1 + str2 == str2 + str1: #strings are symmetric
+            gcd = fractions.gcd(len(str1), len(str2))
+            # print("Gcd:", gcd)
+            return str1[:gcd]
+        else:
+            return ""
