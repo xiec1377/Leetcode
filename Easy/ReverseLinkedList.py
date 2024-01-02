04af3343193c817d0abb005d0b85d4588fbe8492
Catherine Xie <catherinexie2014@gmail.com>
2024-01-02 18:52:32 -0500

    Edited ReverseLinkedList.py

--- Easy/ReverseLinkedList.py
+++ Easy/ReverseLinkedList.py
@@ -28,10 +28,11 @@
 
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     def reverseList(self, head):
         """
@@ -45,6 +46,5 @@ class Solution(object):
             cur.next = prev
             prev = cur
         return prev
-
 
     
