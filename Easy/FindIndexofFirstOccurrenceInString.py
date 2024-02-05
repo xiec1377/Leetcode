5bb7fd9d50008e735870b95a0f23ddaa15046429
Catherine Xie <catherinexie2014@gmail.com>
2024-02-05 14:13:13 -0500

    Create FindIndexofFirstOccurrenceInString.py

---
+++ Easy/FindIndexofFirstOccurrenceInString.py
@@ -0,0 +1,41 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+# Example 2:
+
+# Input: haystack = "leetcode", needle = "leeto"
+# Output: -1
+# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+# Constraints:
+
+# 1 <= haystack.length, needle.length <= 104
+# haystack and needle consist of only lowercase English characters.
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            # print("i: ", i)
+            for j in range(len(needle)):
+                if needle[j] != haystack[i+j]:
+                    break
+                if j == len(needle) - 1:
+                    return i
+        return -1
+
+        # runtime 7 ms
+        # beats 95.84% of users
