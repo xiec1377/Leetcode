422277d0cac3f7181bdbaf5e0b9f3d0262e69bf4
Catherine Xie <catherinexie2014@gmail.com>
2024-02-06 16:51:25 -0500

    Create MinCostClimbingStairs.py

---
+++ Medium/MinCostClimbingStairs.py
@@ -0,0 +1,58 @@
+# You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+# You can either start from the step with index 0, or the step with index 1.
+
+# Return the minimum cost to reach the top of the floor.
+
+ 
+
+# Example 1:
+
+# Input: cost = [10,15,20]
+# Output: 15
+# Explanation: You will start at index 1.
+# - Pay 15 and climb two steps to reach the top.
+# The total cost is 15.
+# Example 2:
+
+# Input: cost = [1,100,1,1,1,100,1,1,100,1]
+# Output: 6
+# Explanation: You will start at index 0.
+# - Pay 1 and climb two steps to reach index 2.
+# - Pay 1 and climb two steps to reach index 4.
+# - Pay 1 and climb two steps to reach index 6.
+# - Pay 1 and climb one step to reach index 7.
+# - Pay 1 and climb two steps to reach index 9.
+# - Pay 1 and climb one step to reach the top.
+# The total cost is 6.
+
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        # dp = 0
+        # dp1 = 0
+        # dp2 = 0
+        # for i in range(2, len(cost)+1):
+        #     twoSteps = dp2 + cost[i-2]
+        #     oneStep = dp1 + cost[i-1]
+        #     dp = min(twoSteps, oneStep)
+        #     # print("dp:", dp)
+        #     dp2 = dp1
+        #     dp1 = dp
+        # return dp
+
+        # initialize memoization
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        # for each step, memoize the cost of current step and min cost of either previous step or previous two steps
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-2], dp[i-1])
+        # return the min of last step and second last step
+        return min(dp[-1], dp[-2])
+
