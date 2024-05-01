6b4d001e95a409f5ba0607256520ad63c3cc95cd
Catherine Xie <catherinexie2014@gmail.com>
2024-05-01 18:38:49 -0400

    Time: 526 ms (10.05%), Space: 13.9 MB (81.13%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -9,22 +9,26 @@ class Solution(object):
         # use djikstra's algo for shortest path
         visited = set()
         heap = []
-        heapq.heappush(heap,[0,k])
+        heapq.heappush(heap, [0, k])
         graph = defaultdict(list)
 
-        for u,v,w in times:
-            graph[u].append((v,w))
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        print("graph:", graph)
 
         while heap:
-            wei,node = heapq.heappop(heap)
-            if node in visited:
+            print("in heap")
+            weight, u = heapq.heappop(heap)
+            print(u, weight)
+            if u in visited:
                 continue
-            visited.add(node)
-            if len(visited)==n:
-                return wei
-
-            for nei,w in graph[node]:
-                heapq.heappush(heap,[w+wei,nei])
+            visited.add(u)
+            if len(visited) == n:
+                return weight
+            for v, w in graph[u]:
+                print("v, w:", v, w)
+                heapq.heappush(heap, [w+weight, v])
 
         return -1
         
