122942434bca6264a42ff7d2e3597820cbf8dd7b
Catherine Xie <catherinexie2014@gmail.com>
2024-05-02 01:24:21 -0400

    Time: 3664 ms (30.79%), Space: 164.5 MB (22.56%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -42,21 +42,21 @@ class Solution(object):
                
 
 
-        manhattan = lambda p1, p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
-        n, c = len(points), collections.defaultdict(list)
-        for i in range(n):
-            for j in range(i+1, n):
-                d = manhattan(points[i], points[j])
-                c[i].append((d, j))
-                c[j].append((d, i))
-        cnt, ans, visited, heap = 1, 0, [0] * n, c[0]
+        # manhattan = lambda p1, p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        graph = defaultdict(list)
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+                d = self.getManhattanDistance(points[i][0], points[i][1], points[j][0], points[j][1])
+                graph[i].append([d, j])
+                graph[j].append([d, i])
+        cnt, ans, visited, heap = 1, 0, [0] * len(points), graph[0]
         visited[0] = 1
         heapq.heapify(heap)
         while heap:
             d, j = heapq.heappop(heap)
             if not visited[j]:
                 visited[j], cnt, ans = 1, cnt+1, ans+d
-                for record in c[j]: heapq.heappush(heap, record)
-            if cnt >= n: break        
+                for record in graph[j]: heapq.heappush(heap, record)
+            if cnt >= len(points): break        
         return ans
         
