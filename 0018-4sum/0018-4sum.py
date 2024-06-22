e496bea3a65d2451b8ba8f31121c93658d486b76
Catherine Xie <cxie@justworks.com>
2024-06-22 19:29:03 -0400

    Time: 889 ms (23.34%), Space: 11.6 MB (44.84%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -9,21 +9,15 @@ class Solution(object):
         results = set()
         for i in range(len(nums)):
             for j in range(len(nums) -1, 0, -1):
-                # print "i", i, "j", j
                 l = i + 1
                 r = j - 1
                 while l < r:
-                    # print "l:", l, "r:", r
                     sum = nums[i] + nums[j] + nums[l] + nums[r]
-                    # print "sum:", sum
                     if sum < target:
-                        # print "sum < target"
                         l += 1
                     elif sum > target:
-                        # print "sum > target"
                         r -= 1
                     else:
-                        # print "sum == target"
                         quad = tuple([nums[i], nums[l], nums[r], nums[j]])
                         results.add(quad)
                         r -= 1
