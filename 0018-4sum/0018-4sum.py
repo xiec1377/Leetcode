95b7fc565b725ca328ce96323fda334a1fb2e953
Catherine Xie <cxie@justworks.com>
2024-06-28 00:10:09 -0400

    Time: 556 ms (69.33%), Space: 11.7 MB (42.98%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,24 +5,38 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        nums.sort()
-        results = set()
-        for i in range(len(nums)):
-            for j in range(len(nums) -1, 0, -1):
-                l = i + 1
-                r = j - 1
-                while l < r:
-                    sum = nums[i] + nums[j] + nums[l] + nums[r]
-                    if sum < target:
-                        l += 1
-                    elif sum > target:
-                        r -= 1
-                    else:
-                        quad = tuple([nums[i], nums[l], nums[r], nums[j]])
-                        results.add(quad)
-                        r -= 1
-                        l += 1
-        return results
-
-
         
+        nums.sort()
+        result = []
+
+        for a in range(len(nums) - 3):
+            if a > 0 and nums[a] == nums[a - 1]:
+                continue
+
+            for b in range(a + 1, len(nums) - 2):
+                if b > a + 1 and nums[b] == nums[b - 1]:
+                    continue
+                    
+                c = b + 1
+                d = len(nums) - 1
+
+                while c < d:
+                    total = nums[a] + nums[b] + nums[c] + nums[d]
+
+                    if total == target:
+                        result.append([nums[a], nums[b], nums[c], nums[d]])
+
+                        while c < d and nums[c] == nums[c + 1]:
+                            c += 1
+
+                        while c < d and nums[d] == nums[d - 1]:
+                            d -= 1
+
+                        c += 1
+                        d -= 1
+                    elif total < target:
+                        c += 1
+                    else:
+                        d -= 1
+        
+        return result
