d70c3996feec6a6c7cfad4868a6a6ac5a8694faf
Catherine Xie <cxie@justworks.com>
2024-06-30 18:24:56 -0400

    Time: 89 ms (96.97%), Space: 17.6 MB (21.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -10,17 +10,9 @@ class Solution(object):
         result.append(intervals[0])
         for start, end in intervals:
             prev_start, prev_end = result[-1][0], result[-1][1]
-            # print("start:", start, "end:", end)
-            # print("prev_end", prev_end)
             if start <= prev_end and start >= prev_start:
-                # print("in case")
-                print([prev_start, max(end, prev_end)])
                 result.pop() 
                 result.append([prev_start, max(end, prev_end)])
             else:
-                # print("in else")
-                print([start, end])
                 result.append([start, end])
-            
-            # print("result:", result)
         return result
