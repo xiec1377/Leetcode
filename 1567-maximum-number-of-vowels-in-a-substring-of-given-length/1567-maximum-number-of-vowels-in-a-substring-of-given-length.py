b46f1ce7162550a29b5418b89b0c5be9146f6fad
Catherine Xie <cxie@justworks.com>
2024-07-06 18:21:53 -0400

    Time: 178 ms (38.35%), Space: 15.6 MB (49.87%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,39 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-
-        # sliding window probelm
-        # keep track of beginning of window
-        # iterate through char in s
-        # keep track of max vowels
-
         vowels = ['a', 'e', 'i', 'o', 'u']
         sumVowels = 0
         maxVowels = float('-inf')
 
         for i in range(k):
-            # print("iii:", i)
             if s[i] in vowels:
                 sumVowels += 1
                 maxVowels = max(maxVowels, sumVowels)
-            
-        
-        # print("maxvowels", maxVowels)
         if len(s) == k:
             return maxVowels
-
-        start = 1
         for i in range(k, len(s)):
-            # print("char:", s[i])
             if s[i] in vowels:
                 sumVowels += 1
             if s[i-k] in vowels:
                 sumVowels -= 1
-            # print("sumvowels", sumVowels)
             maxVowels = max(maxVowels, sumVowels)
-
-            # print("max:", maxVowels)
-            # start += 1
         return maxVowels
                 
 
