54e333f3eedcef156d086070357947ed52a97bec
Catherine Xie <cxie@justworks.com>
2024-07-16 22:57:25 -0400

    Time: 178 ms (59.03%), Space: 12.8 MB (78.1%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # two pointers
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            # calculate middle pointer index
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+        
