61ced305fb39ff400c1f18f077168b07c3f9aa6b
Catherine Xie <cxie@justworks.com>
2024-09-29 20:27:21 -0400

    Time: 21 ms (30.23%), Space: 11.4 MB (99.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        slow = 0
-        fast = 0 
-        while slow < len(nums) and fast < len(nums) - 1:
-            fast = slow + 1
-            if nums[slow] > nums[fast]:
-                tmp = nums[slow]
-                nums[slow] = nums[fast]
-                nums[fast] = tmp
-                while slow > 0:
-                    if nums[slow] < nums[slow-1]:
-                        tmp = nums[slow]
-                        nums[slow] = nums[slow-1]
-                        nums[slow-1] = tmp
-                    slow -= 1
-                slow = fast
+        red = 0
+        white = 0
+        for num in nums:
+            if num == 0:
+                red += 1
+            elif num == 1:
+                white += 1
+    
+        for i in range(len(nums)):
+            if i < red:
+                nums[i] = 0
+            elif red <= i < red + white:
+                nums[i] = 1
             else:
-                slow += 1
+                nums[i] = 2
+
+            
