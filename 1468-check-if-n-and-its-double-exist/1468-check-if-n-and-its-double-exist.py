311501db964a450ea68b0c52016571d72f9ab7a5
Catherine Xie <cxie@justworks.com>
2024-11-30 23:14:59 -0500

    Time: 75 ms (8.2%), Space: 12.4 MB (6.03%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -7,24 +7,9 @@ class Solution(object):
         if arr.count(0) > 1:
             return True
         dic = {}
-        # for i, num in enumerate(arr):
-        #     print("i:", i)
-        #     print("num:", num)
-        #     dic[i] = num
-        
-        # for key, val in dic.items():
-        #     print("key:", key)
-        #     print("val:", val)
-        #     if i in dic.keys() and i != key and dic[i] == dic[key] * 2:
-        #         print("we here")
-        #         return True
-        # return False
-
         for num in arr:
             dic[num] = num * 2
         print("dic:", dic)
-        # if 0 in dic.keys():
-        #     return True
         for key, val in dic.items():
             if key != val and (val in dic.keys() or key in dic.values()):
                 return True
