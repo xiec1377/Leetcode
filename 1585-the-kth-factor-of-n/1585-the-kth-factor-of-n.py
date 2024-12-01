acdfd79e6ddeda2b5ed754db20f68f63eff84300
Catherine Xie <cxie@justworks.com>
2024-12-01 11:33:07 -0500

    Time: 0 ms (100%), Space: 12 MB (5.34%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
@@ -6,13 +6,22 @@ class Solution(object):
         :rtype: int
         """
         # list of factors
-        factors = []
+        # factors = []
+        # for i in range(1, n+1):
+        #     if n % i == 0:
+        #         factors.append(i)
+        
+        # if 1 <= k <= len(factors):
+        #     return factors[k-1]
+        # else:
+        #     return -1
+        idx = 0
         for i in range(1, n+1):
             if n % i == 0:
-                factors.append(i)
+                idx += 1
+                if idx == k:
+                    return i
+        return -1
         
-        if 1 <= k <= len(factors):
-            return factors[k-1]
-        else:
-            return -1
+
         
