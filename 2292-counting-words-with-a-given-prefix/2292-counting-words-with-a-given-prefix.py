69e3759b7ec54e491bac56554c05afba11b358f6
Catherine Xie <cxie@justworks.com>
2025-01-09 01:31:18 -0500

    Time: 0 ms (100%), Space: 12.3 MB (51.65%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
@@ -7,7 +7,6 @@ class Solution(object):
         """
         ans = 0
         for word in words:
-            print("word[0:len(pref)]:", word[0:len(pref)] )
             if word[0:len(pref)] == pref:
                 ans += 1
         return ans
