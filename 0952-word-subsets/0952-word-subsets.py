53e37433dc30773a8a0997eb293378274a949bc9
Catherine Xie <cxie@justworks.com>
2025-01-10 18:47:16 -0500

    Time: 837 ms (88.06%), Space: 16 MB (17.91%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def wordSubsets(self, words1, words2):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :rtype: List[str]
+        """
+        freq = {}
+        ans = []
+        for wordb in words2:
+            for c, n in Counter(wordb).items():
+                if c not in freq.keys() or freq[c] < n:
+                    freq[c] = n
+
+        for worda in words1:
+            isUniversal = True
+            count = Counter(worda)
+            for c, n in freq.items():
+                if count[c] < n:
+                    isUniversal = False
+                    break
+            if isUniversal:
+                ans.append(worda)
+        return ans
