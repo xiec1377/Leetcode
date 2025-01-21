a0266684d37df3e3fb1106f90bedebd6fa896566
Catherine Xie <cxie@justworks.com>
2025-01-21 01:38:21 -0500

    Time: 80 ms (89.29%), Space: 20.6 MB (67.86%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def gridGame(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        sum_top = sum(grid[0])
+        sum_bot = 0
+        result = float('inf')
+        for i in range(len(grid[0])):
+            sum_top -= grid[0][i]
+            result = min(result, max(sum_top, sum_bot))
+            sum_bot += grid[1][i]
+        return result
+    
