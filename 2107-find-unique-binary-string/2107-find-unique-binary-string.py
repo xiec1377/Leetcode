69a393f2e15c62d05434909b4a82b28601b8c79e
Catherine Xie <cxie@justworks.com>
2025-02-20 01:44:19 -0500

    Time: 0 ms (100%), Space: 17.9 MB (52.45%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.py
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.py
@@ -1,10 +1,13 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         result = ''
+        # diagonal flipping technique - ensures every bit at position (i, i) is unique
         for i in range(len(nums)):
             if nums[i][i] == '0':
                 result += '1'
             else:
                 result +='0'
         return result
+        # Time: O(n)
+        # Space: O(1)
         
