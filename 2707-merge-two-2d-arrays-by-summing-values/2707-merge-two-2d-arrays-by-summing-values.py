a9b1514000c90cebe63b1eb31b01a00c3d56f1a2
Catherine Xie <cxie@justworks.com>
2025-07-28 12:18:06 -0400

    Time: 4 ms (9.18%), Space: 18.3 MB (17.84%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        myDict = {}
+        res = []
+        for key, val in nums1:
+            print("key:", key)
+            print("val:", val)
+            if key in myDict:
+                myDict[key] += val
+            else:
+                myDict[key] = val
+            
+        for key, val in nums2:
+            if key in myDict:
+                myDict[key] += val
+            else:
+                myDict[key] = val
+        
+        for key in myDict.keys():
+            res.append([key, myDict[key]])
+        return sorted(res, key=lambda x: x[0])
+
+        
