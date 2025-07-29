d7c5cee62251da38d2097c1542e6b09086437aff
Catherine Xie <cxie@justworks.com>
2025-07-29 19:34:13 -0400

    Time: 0 ms (100%), Space: 17.9 MB (32.24%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,12 +1,9 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        res = 5
         if n == 1:
             return 1
-        if n == 2:
-            return 5
-        for i in range(3, n + 1):
-            res += 4 + ((i - 2) * 4)
+        res = 1
+        res += 2  * n * (n - 1)
         return res
             
 
