845eed6dfa7157eee890153aa0d33919a0ffdef6
Catherine Xie <cxie@justworks.com>
2025-07-29 19:34:52 -0400

    Time: 0 ms (100%), Space: 17.7 MB (48.81%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -6,6 +6,14 @@ class Solution:
         res += 2  * n * (n - 1)
         return res
             
+        # res = 5
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 5
+        # for i in range(3, n + 1):
+        #     res += 4 + ((i - 2) * 4)
+        # return res
 
 
         
