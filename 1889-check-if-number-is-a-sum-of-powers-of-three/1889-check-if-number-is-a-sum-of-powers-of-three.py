c33c32acd3d0c0cc6820689ba3079940b6811f01
Catherine Xie <cxie@justworks.com>
2025-07-29 15:49:53 -0400

    Time: 0 ms (100%), Space: 17.9 MB (20.38%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,7 +1,22 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
+        # cannot have 2 in base 3
         if n == 0:
             return True
         if n % 3 == 2:
             return False
         return self.checkPowersOfThree(n // 3)
+        # # log3 n until whole positive number reached, else false
+        # res = math.log(n, 3)
+        # print("res:", res)
+        # if res.is_integer():
+        #     print("here")
+        #     return True
+        # else:
+        #     diff = n - (3 ** math.floor(res))
+        #     print("diff:", diff)
+        #     if diff < 0:
+        #         return False
+        #     return self.checkPowersOfThree(diff)
+
+        
