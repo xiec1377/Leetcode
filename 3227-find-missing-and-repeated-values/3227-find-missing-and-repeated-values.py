4b9c6878b6b7024288eecb960f960c1ffbdb3487
Catherine Xie <cxie@justworks.com>
2025-08-01 17:49:58 -0400

    Time: 31 ms (14.57%), Space: 18.3 MB (21.12%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -2,17 +2,13 @@ class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         myDict = defaultdict(int)
         arr = [0] * len(grid) ** 2
-        print("len arr:", len(arr))
         row = len(grid)
         res = [0] * 2
         for i in range(row):
             for j in range(row):
-                print("i:", i)
-                print("j:", j)
                 print(grid[i][j])
                 arr[grid[i][j] - 1] += 1
                 if arr[grid[i][j] - 1] >= 2:
-                    print("grid[i][j]:", grid[i][j] - 1)
                     res[0] = grid[i][j]
 
         for i in range(len(arr)):
