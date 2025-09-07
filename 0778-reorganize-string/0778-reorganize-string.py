3ca98d648d64d55ce1c9d5d5c3ea3b890a1800c3
Catherine Xie <cxie@justworks.com>
2025-09-07 00:08:54 -0400

    Time: 3 ms (47.54%), Space: 17.8 MB (46.51%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,27 +1,21 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         freq = Counter(s) # frequency hash table with highest freq first 
-        print(freq)
         maxheap = []
         for key in freq.keys():
             heapq.heappush(maxheap, (-freq[key], key))
-        
-        print(maxheap)
         result = ""
         prev_char = ''
         prev_count = 0
         while maxheap:
             count, char = heapq.heappop(maxheap)
-            print("char:", char, "count:,", count)
             if prev_count < 0: # must push to heap for previous now because if we push right when iterating previous, it could result in time exceeded
                 heapq.heappush(maxheap, (prev_count, prev_char))
             if char != prev_char:
-                print("here")
                 result += char
                 count += 1
                 prev_count = count
                 prev_char = char
-        
         if len(result) != len(s): # must be same length
             return ""
         else:
