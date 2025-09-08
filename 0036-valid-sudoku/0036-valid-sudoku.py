36564f4380cf662eeeb22de05ac2accc599a6184
Catherine Xie <cxie@justworks.com>
2025-09-08 15:10:56 -0400

    Time: 33 ms (5.67%), Space: 18 MB (5.05%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,25 +4,18 @@ class Solution:
         cols = len(board[0])
         squares = [[] for _ in range(9)] # 9 for each 3x3 square
         colarray = [[] for _ in range(9)]
-        print("colarray init:", colarray)
         for r in range(rows):
             rowsarray = []
             for c in range(cols):
-                print("r:", r, "c:", c)
-                print(board[r][c])
                 if board[r][c] in rowsarray:
-                    print("row false")
                     return False
                 else:
                     if board[r][c] >= '1' and board[r][c] <= '9':
                         rowsarray.append(board[r][c])
                 if board[r][c] in colarray[c]:
-                    print("colarray[c] false:", colarray[c])
-                    print("col false")
                     return False
                 else:
                     if board[r][c] >= '1' and board[r][c] <= '9':
-                        print("colarray[c] true:", colarray[c])
                         colarray[c].append(board[r][c]) 
                 
                 rowsquare = r // 3
@@ -37,7 +30,5 @@ class Solution:
                     if board[r][c] >= '1' and board[r][c] <= '9':
                         squares[square_index].append(board[r][c])
                         print("quares[colsquare + rowsquare]:", squares[square_index])
-            print("rowsarray:", rowsarray)
         
-        print("colarray:", colarray)
         return True
