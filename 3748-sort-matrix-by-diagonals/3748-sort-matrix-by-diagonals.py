726cb4fce6fd8c3db2f6f98e34b1ca8e8bc10380
Catherine Xie <cxie@justworks.com>
2025-09-10 11:38:37 -0400

    Time: 25 ms (5.63%), Space: 17.9 MB (47.33%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        # bottom triangle
+        for i in range(n):
+            tmp = [grid[i+j][j] for j in range(n-i)]
+            print("tmp:", tmp)
+            tmp.sort(reverse=True)
+            for j in range(n - i):
+                print("j:", j)
+                grid[i+j][j] = tmp[j]
+        # top triangle
+        for i in range(1, n): # don't include middle diagonal
+            tmp = [grid[j][i+j] for j in range(n-i)]
+            tmp.sort()
+            for j in range(n-i):
+                grid[j][i+j] = tmp[j]
+        return grid
+        
