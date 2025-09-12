b6a53ce8dba62fc1e26053be71b144d22293ec8c
Catherine Xie <cxie@justworks.com>
2025-09-12 15:51:45 -0400

    Time: 614 ms (5%), Space: 55.2 MB (5.39%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int rows;
+    private int cols;
+    public int numIslands(char[][] grid) {
+        int numislands = 0;
+        rows = grid.length;
+        cols = grid[0].length;
+        Set<String> visited = new HashSet<>();
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                String pos = r + ", " + c;
+                if (grid[r][c] == '1' && !visited.contains(pos)) {
+                    numislands++;
+                    System.out.println("here");
+                    bfs(grid, r, c, visited);
+                }
+            }
+        }
+        return numislands;
+
+    }
+
+    private void bfs(char[][] grid, int r, int c, Set<String> visited) {
+        System.out.println("bfs...");
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r, c});
+
+        while (!q.isEmpty()) {
+            int[] coord = q.poll();
+            int row = coord[0];
+            int col = coord[1];
+            for (int[] direction : directions) {
+                System.out.println("direction:" + direction[0] + ", " + direction[1]);
+                int newr = row + direction[0];
+                int newc = col + direction[1];
+                if (newr >= 0 && newr < rows && newc >= 0 && newc < cols && grid[newr][newc] == '1' && !visited.contains(newr + ", " + newc)){
+                    q.add(new int[]{newr, newc});
+                    visited.add(newr + ", " + newc);
+                }
+            }
+        }
+        
+    }
+}
