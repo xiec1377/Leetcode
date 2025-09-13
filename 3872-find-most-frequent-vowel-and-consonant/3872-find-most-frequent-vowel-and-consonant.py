3ad9af99edd0c7f2d85fcd3b22e2828b151f7dec
Catherine Xie <cxie@justworks.com>
2025-09-13 14:35:22 -0400

    Time: 7 ms (15.17%), Space: 17.5 MB (99.8%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -2,8 +2,7 @@ class Solution:
     def maxFreqSum(self, s: str) -> int:
         conMap = defaultdict(int)
         vowelMap = defaultdict(int)
-        maxCon = float('-inf')
-        maxVow = float('-inf')
+        maxCon, maxVow = 0, 0
         for c in s:
             if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c =='u':
                 vowelMap[c] += 1
@@ -11,11 +10,6 @@ class Solution:
             else:
                 conMap[c] += 1
                 maxCon = max(maxCon, conMap[c])
-            
-        if maxCon == float('-inf'):
-            maxCon = 0
-        if maxVow == float('-inf'):
-            maxVow = 0
         return maxCon + maxVow
 
         
