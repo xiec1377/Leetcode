21ae363f2cd19448bce8b0af53046562483acf1d
Catherine Xie <cxie@justworks.com>
2025-09-13 14:24:22 -0400

    Time: 42 ms (31.42%), Space: 18.1 MB (79.76%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        numVowels = 0
+        vowels = set()
+        vowels.update(['a', 'e', 'i', 'o', 'u'])
+        for c in s:
+            if c in vowels:
+                numVowels += 1
+        return numVowels % 2 != 0 or numVowels % 2 == 0 and numVowels > 0
