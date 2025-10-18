301400835cd2ef20281093b5e64609aecc543e72
Catherine Xie <cxie@justworks.com>
2025-10-18 02:10:49 -0400

    Time: 23 ms (55.31%), Space: 18.4 MB (40.87%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,13 +3,13 @@ class Solution:
         rows  = len(grid)
         cols = len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        visited = []
+        visited = set()
         res = 0
 
         def bfs(r, c):
             q = deque()
             q.append(((r, c), 1))
-            visited.append((r, c))
+            visited.add((r, c))
             area = 1
             while q:
                 item = q.popleft()
@@ -19,7 +19,7 @@ class Solution:
                     newr = row + x
                     newc = col + y
                     if 0 <= newr < rows and 0 <= newc < cols and grid[newr][newc] == 1 and (newr, newc) not in visited:
-                        visited.append((newr, newc))
+                        visited.add((newr, newc))
                         q.append(((newr, newc), size + 1))
                         area += 1
             return area
