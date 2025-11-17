fb2bef2eea02e94b93d4211d6756f42151bf9c5d
Catherine Xie <cxie@justworks.com>
2025-11-17 17:08:44 -0500

    Time: 39 ms (29.65%), Space: 32 MB (20.22%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -8,15 +8,11 @@ class Solution(object):
         myMap = defaultdict(int)
         res = []
         for i in range(len(s)-9):
-            print("s[i", s[i])
             j = i + 10
             if j <= len(s): 
-                # print("s[j\]", s[j])
                 k = s[i:j]
-                print("k substring:", k)
                 myMap[k] += 1
                 if myMap[k] > 1 and k not in res:
-                    print("here")
                     res.append(k)
 
         return res
