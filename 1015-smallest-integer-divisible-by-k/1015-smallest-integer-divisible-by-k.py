c05a827ec458ac8a8bb8c7c4cfa02b65ebcd494a
Catherine Xie <cxie@justworks.com>
2025-11-24 22:07:20 -0500

    Time: 15 ms (40.31%), Space: 13.9 MB (49.61%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -7,7 +7,7 @@ class Solution(object):
         remainder = 0
         for i in range(1, k+1):
             remainder = (remainder * 10 + 1) % k
-            print("remainder:", remainder)
+            # print("remainder:", remainder)
             if remainder == 0:
                 return i
         return -1
